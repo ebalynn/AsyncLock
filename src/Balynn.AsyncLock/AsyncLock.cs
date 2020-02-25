455451c2d200b1d69b7a16656aa17c0ab59ccf8e
Elliot Balynn <elliot@balynn.com>
2020-02-25 15:05:38 +0000

    Initial commit

---
+++ src/Balynn.AsyncLock/AsyncLock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Balynn.AsyncLock
+{
+    public class AsyncLock : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+        private async Task Enter()
+        {
+            await _semaphore.WaitAsync();
+        }
+
+        private void Exit()
+        {
+            _semaphore.Release();
+        }
+
+        public void Dispose()
+        {
+            _semaphore?.Dispose();
+        }
+
+        public static async Task<AsyncLock> EnterAsync()
+        {
+            var asyncLock = new AsyncLock();
+            await asyncLock.Enter();
+
+            return asyncLock;
+        }
+    }
+}
